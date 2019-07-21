b9a798342c0c96dbf5dd94d4ba17a783e07836a2
robert21-meet <robert21@meet.mit.edu>
2019-07-21 16:33:24 +0300

    Stopping here

---
+++ lab_1.py
@@ -0,0 +1,19 @@
+import turtle
+turtle.left(90)
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(100)
+turtle.penup()
+turtle.goto(200,100)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(70)
+turtle.penup()
+turtle.
+turtle.
+turtle.
