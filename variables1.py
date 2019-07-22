e3230a6685d1e159ff92674739ba11103be21e1b
robert21-meet <robert21@meet.mit.edu>
2019-07-22 14:20:30 +0300

    Stopping here

---
+++ variables1.py
@@ -0,0 +1,18 @@
+a=5
+b=10
+a=b
+b=a
+print (a)
+print (b)
+
+my_name="robert"
+print ("my name is " + my_name)
+
+my_age = "15"
+print('I am ' + my_age + 'years old')
+
+score = 4
+count = 5
+total = score * count
+print(total)
+
